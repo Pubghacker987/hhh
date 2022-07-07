7e50ed5ead3872a84994db846e70cefd1290da7e
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-07 12:19:21 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,8 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from bot.config import Messages, BotCommands
 
 
 @Client.on_message(filters.command(["start"]) & filters.private)
