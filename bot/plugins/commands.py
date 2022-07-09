8c35f965c1eb7e88c7a47623a58f92431f322db2
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-09 09:13:55 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,10 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import CallbackQuery, Message
 
-from vidmergebot.bot_class import VidMergeBot
-from vidmergebot.utils.constants import Constants
-from vidmergebot.utils.ikb import ikb
-from vidmergebot.vars import Vars
+
 
 
 @Client.on_message(
@@ -13,7 +10,7 @@ from vidmergebot.vars import Vars
 async def start_bot(_, m: Message):
     return await m.reply_text(
         Messages.START_MSG.format(m.from_user.first_name, Messages.CAPTION),
-        reply_markup=ikb(Messages.start_kb),
+        reply_markup=Messages.START_BUTTONS,
         disable_web_page_preview=True,
         quote=True,
     )
@@ -25,7 +22,7 @@ async def start_bot(_, m: Message):
 async def help_bot(_, m: Message):
     return await m.reply_text(
         Messages.HELP_MSG,
-        reply_markup=ikb(Messages.page1_help_kb),
+        reply_markup=Messages.HELP_BUTTONS,
         disable_web_page_preview=True,
     )
 
